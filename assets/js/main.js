f4148eb83a04afada4f895f7feccf80c94a3201e
giuseppevignanello <peppe.vignanello@gmail.com>
2023-03-15 17:09:37 +0100

    problem analysis

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,19 @@
 // Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
+
+
+
+// create array with grocery items inside 
+//take an element list from HTML and save it in a variable  
+//create a while loop to slide the array
+    // create an element for HTML 
+    // add element from array in HTMLelement 
+    //append the element to HTMLlist 
+
+
+
+// tools
+// - console.log();
+// getElementbyID/querySelector
+//array
+// while
+//let-const
